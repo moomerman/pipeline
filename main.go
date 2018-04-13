75f27389dee953638ff48eb53524f7694d53aa52
Richard Taylor <moomerman@gmail.com>
2018-04-13 15:56:50 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	githubhook "gopkg.in/rjz/githubhook.v0"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	secret := []byte("don't tell!")
+	hook, err := githubhook.Parse(secret, r)
+	if err != nil {
+		return
+	}
+
+	fmt.Println(hook.Event)
+	fmt.Println(hook.Id)
+
+	fmt.Fprintf(w, "Hi there")
+}
